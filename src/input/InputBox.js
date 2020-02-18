52f17591ad6714dbdc88965440fb1ba587e92ad7
Joseph Montesano <jmmmontesano@gmail.com>
2020-02-17 21:21:02 -0500

    updated

--- src/input/InputBox.js
+++ src/input/InputBox.js
@@ -5,24 +5,27 @@ import './InputBox.css'
 class InputBox extends Component {
     render(){
         return(
-                    <div>
+             <div>
+                    <div className="colors">
                         <label htmlFor="black" className="black">Black</label>
-                        <input type="text" name="black" id="black" placeholder="0"/>
+                        <input className="colorInput" type="text" name="black" id="black" placeholder="0"/>
      
                         <label htmlFor="blue" className ="blue color">Blue</label>
-                        <input type="text"  name="blue" id="blue" placeholder="0"/>
+                        <input className="colorInput" type="text"  name="blue" id="blue" placeholder="0"/>
   
                         <label htmlFor="orange" className="orange color">Orange</label>
-                        <input type="text"  name="orange" id="orange" placeholder="0"/> 
-
+                        <input className="colorInput" type="text"  name="orange" id="orange" placeholder="0"/> 
+                    </div>
+                        
+                    <div className="additionalInput">
                         <label htmlFor="modifier">Modifier</label>
                         <input type="text" name="modifier" id="modifier" placeholder="(+1, -1)"/>
     
                         <label htmlFor="desiredRoll">Desired Roll</label>
                         <input type="text" name="desiredRoll" id="desiredRoll" placeholder="0"/>
-
-   
                     </div>
+   
+             </div>
         )
     }
 }
--- src/input/NameBox.js
+++ src/input/NameBox.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './NameBox.css'
 
 class NameBox extends Component {
     render(){
         return(
-            <div>
+            <div className="nameOfRoll">
                     <label htmlFor="nameOfRoll">Name of the roll?
                         <input type="text" name = "nameOfRoll" id = "nameOfRoll" placeholder = "Spear. Sword, Armor dodge rolls etc"/>
                     </label>
