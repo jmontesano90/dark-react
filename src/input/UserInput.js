de6a7994a0c8baf9511d28b6c7ceaa18e5434972
Joseph Montesano <jmmmontesano@gmail.com>
2020-03-24 15:58:03 -0400

    testing

--- src/input/UserInput.js
+++ src/input/UserInput.js
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 import InputBox from "./InputBox";
 import "./UserInput.css";
+import "../ResultContext";
 
 class UserInput extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      black: 0,
-      blue: 0,
-      orange: 0,
-      modifier: 0,
-      desiredRoll: 0,
-      nameOfRoll: null
+      result: {
+        black: 0,
+        blue: 0,
+        orange: 0,
+        modifier: 0,
+        desiredRoll: 0,
+        nameOfRoll: null
+      }
     };
     this.handleChange = this.handleChange.bind(this);
   }
 
+  // static contextType = ResultContext;
+
   handleChange(event) {
     this.setState({
-      [event.target.name]: event.target.value
+      result: {
+        [event.target.name.toString()]: Number(event.target.value)
+      }
     });
-    console.log(event.target.name, event.target.value);
+    console.log(this.state.result);
   }
 
   handleSubmit(event) {
     event.preventDefault();
-    console.log("I was clicked");
+    // console.log("I was clicked", this.state.result);
   }
   render() {
     return (
